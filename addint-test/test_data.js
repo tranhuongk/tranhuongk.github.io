157a0fa271feab1f716e82c0f3bf3a7013708900
tranhuongk <huongtv.uet@gmail.com>
2020-01-14 09:57:15 +0700

    update data

--- addint-test/test_data.js
+++ addint-test/test_data.js
@@ -12,17 +12,17 @@ test_data = [
         "lon": "105.7875823"
     },
     {
-        "address": "23 Hồ Tùng Mậu ",
+        "address": "23 Hồ Tùng Mậu",
         "lat": "21.0365849",
         "lon": "105.7776196"
     },
     {
-        "address": "19 Hồ Tùng Mậu ",
+        "address": "19 Hồ Tùng Mậu",
         "lat": "21.036506",
         "lon": "105.777983"
     },
     {
-        "address": "133 Xuân Thuỷ ",
+        "address": "133 Xuân Thuỷ",
         "lat": "21.036427",
         "lon": "105.784883",
         "note": "ngõ",
@@ -146,7 +146,7 @@ test_data = [
         "lon": "105.7899513"
     },
     {
-        "address": "18 Nguyễn Phong  Sắc",
+        "address": "18 Nguyễn Phong Sắc",
         "lat": "21.0384628",
         "lon": "105.7904241"
     },
@@ -158,7 +158,7 @@ test_data = [
         "deviation": "50"
     },
     {
-        "address": "70 Nguyễn Phong Sắc ",
+        "address": "70 Nguyễn Phong Sắc",
         "lat": "21.0395202",
         "lon": "105.7905053"
     },
@@ -173,7 +173,7 @@ test_data = [
         "lon": "105.7917408"
     },
     {
-        "address": "153 Trần Đăng Ninh ",
+        "address": "153 Trần Đăng Ninh",
         "lat": "21.038708",
         "lon": "105.7923841"
     },
@@ -198,7 +198,7 @@ test_data = [
         "lon": "105.7826896"
     },
     {
-        "address": "22 Trần Quốc Hoàn ",
+        "address": "22 Trần Quốc Hoàn",
         "lat": "21.0415581",
         "lon": "105.7868781"
     },
@@ -218,7 +218,7 @@ test_data = [
         "lon": "105.7762103"
     },
     {
-        "address": "2 Phố Mai Dịch ",
+        "address": "2 Phố Mai Dịch",
         "lat": "21.0419104",
         "lon": "105.7752869",
         "note": "Nhà nằm ở nút giao (ngã tư)",
@@ -257,8 +257,175 @@ test_data = [
         "lon": "105.7786027"
     },
     {
-        "address": "40 Trần Bình ",
+        "address": "40 Trần Bình",
         "lat": "21.0353069",
         "lon": "105.778699"
+    },
+    {
+        "address": "52 Trần Bình",
+        "lat": "21.0348546",
+        "lon": "105.7787468"
+    },
+    {
+        "address": "95 Trần Bình",
+        "lat": "21.0319984",
+        "lon": "105.7787296"
+    },
+    {
+        "address": "1194 Đường Láng",
+        "lat": "21.0275932",
+        "lon": "105.7996781"
+    },
+    {
+        "address": "1174 Đường Láng",
+        "lat": "21.0272032",
+        "lon": "105.7993868"
+    },
+    {
+        "address": "1028 Đường Láng",
+        "lat": "21.0237896",
+        "lon": "105.7990193"
+    },
+    {
+        "address": "976 Đường Láng",
+        "lat": "21.0224967",
+        "lon": "105.8002048",
+        "note": "Nằm ở nút giao ngã 3",
+        "deviation": "50"
+    },
+    {
+        "address": "938 Đường Láng",
+        "lat": "21.0214719",
+        "lon": "105.8010979"
+    },
+    {
+        "address": "886 Đường Láng",
+        "lat": "21.0200458",
+        "lon": "105.801941"
+    },
+    {
+        "address": "828 Đường Láng",
+        "lat": "21.0190871",
+        "lon": "105.8026428",
+        "note": "Nhà diện tích lớn",
+        "deviation": "50"
+    },
+    {
+        "address": "808 Đường Láng",
+        "lat": "21.0183023",
+        "lon": "105.802968",
+        "note": "Nhà diện tích lớn",
+        "deviation": "50"
+    },
+    {
+        "address": "762 Đường Láng",
+        "lat": "21.0173381",
+        "lon": "105.803449"
+    },
+    {
+        "address": "714 Đường Láng",
+        "lat": "21.0165005",
+        "lon": "105.8041962"
+    },
+    {
+        "address": "69 Nguyễn Khang",
+        "lat": "21.0167159",
+        "lon": "105.8028591",
+        "note": "Nút giao ngã 3",
+        "deviation": "50"
+    },
+    {
+        "address": "71 Nguyễn Khang",
+        "lat": "21.0171251",
+        "lon": "105.8025678",
+        "note": "Nút giao ngã 3",
+        "deviation": "50"
+    },
+    {
+        "address": "83 Nguyễn Khang",
+        "lat": "21.0181026",
+        "lon": "105.8019519"
+    },
+    {
+        "address": "HH1 Dương Đình Nghệ",
+        "lat": "21.0220473",
+        "lon": "105.7874539",
+        "note": "Địa chỉ toà chung cư",
+        "deviation": "50"
+    },
+    {
+        "address": "68 Dương Đình Nghệ",
+        "lat": "21.0221685",
+        "lon": "105.7884605"
+    },
+    {
+        "address": "15 Phạm Hùng",
+        "lat": "21.0292029",
+        "lon": "105.7785726",
+        "note": "Ngã tư, toà nhà lớn",
+        "deviation": "50"
+    },
+    {
+        "address": "18 Phạm Hùng",
+        "lat": "21.0293008",
+        "lon": "105.777166",
+        "note": "Ngã tư, toà nhà lớn",
+        "deviation": "50"
+    },
+    {
+        "address": "16A Phạm Hùng",
+        "lat": "21.0303449",
+        "lon": "105.7788167"
+    },
+    {
+        "address": "5 Phạm Hùng",
+        "lat": "21.0303449",
+        "lon": "105.7788167"
+    },
+    {
+        "address": "8 Phạm Hùng",
+        "lat": "21.0341596",
+        "lon": "105.779948"
+    },
+    {
+        "address": "301 Tô Hiệu",
+        "lat": "21.041578",
+        "lon": "105.7908598"
+    },
+    {
+        "address": "106 Tô Hiệu",
+        "lat": "21.0414171",
+        "lon": "105.7910914"
+    },
+    {
+        "address": "233 Tô Hiệu",
+        "lat": "21.0413665",
+        "lon": "105.7921732",
+        "note": "Ngã 3",
+        "deviation": "50"
+    },
+    {
+        "address": "123 C5 Tô Hiệu",
+        "lat": "21.0414047",
+        "lon": "105.7925618",
+        "note": "Ngã 3, số nhà chứa chữ cái, lô kiot",
+        "deviation": "60"
+    },
+    {
+        "address": "14 Tô Hiệu",
+        "lat": "21.0419484",
+        "lon": "105.7939023",
+        "note": "Diện tích rất lớn (2 trường học)",
+        "deviation": "100"
+    },
+    {
+        "address": "435 Hoàng Quốc Việt",
+        "lat": "21.0460653",
+        "lon": "105.7856036"
+    },
+    {
+        "address": "421 Hoàng Quốc Việt",
+        "lat": "21.0459842",
+        "lon": "105.7860711"
     }
 ]
