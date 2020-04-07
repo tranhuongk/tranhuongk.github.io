8e48aa9a6881a506d265852a09c63b36ffa15ec6
unknown <huongtv.uet@gmail.com>
2020-04-08 02:15:30 +0700

    test interpolation

--- pelias-test/calculate.js
+++ pelias-test/calculate.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
     var data
     var street1
     var street2
-    var tqh = tqh_street
     if (type == null) {
         type = 1;
         window.location.href += `?type=${type}`
@@ -13,9 +12,21 @@ $(document).ready(function () {
     switch (type) {
         case "0":
             data = data0;
-            street1 = xuanthuy_v1
-            street2 = xuanthuy_v2
+            street1 = xuanthuy_street
+            street2 = null
             $("select.custom-select").val("0").change();
+            interpolation_data2.features.forEach(element => {
+                if (element.properties.type == "interpolated") {
+                    let interpolation_marker = new wemapgl.Marker(marker_interpolation.cloneNode(true))
+                        .setLngLat([element.properties.lon, element.properties.lat])
+                        .addTo(map)
+                    let viewPopup = document.createElement("div");
+                    viewPopup.classList.add("viewPopup2");
+                    viewPopup.innerHTML = element.properties.number
+                    let markerElement = interpolation_marker._element;
+                    markerElement.appendChild(viewPopup);
+                }
+            })
             break
         case "2":
             data = data1;
@@ -38,8 +49,8 @@ $(document).ready(function () {
         case "5":
             $("select.custom-select").val("5").change();
             data = data1
-            street1 = xuanthuy_f1
-            street2 = xuanthuy_f2
+            street1 = xuanthuy_street
+            street2 = null
             interpolation_data.features.forEach(element => {
                 if (element.properties.type == "interpolated") {
                     let interpolation_marker = new wemapgl.Marker(marker_interpolation.cloneNode(true))
@@ -113,25 +124,25 @@ $(document).ready(function () {
     });
 
     map.on('load', function () {
-        map.addSource('national-park', {
-            'type': 'geojson',
-            'data': data
-        });
+        // map.addSource('national-park', {
+        //     'type': 'geojson',
+        //     'data': data
+        // });
 
-        map.addLayer({
-            'id': 'park-boundary',
-            'type': 'line',
-            'source': 'national-park',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#035afc',
-                'line-width': 8
-            },
-            'filter': ['==', '$type', 'LineString']
-        });
+        // map.addLayer({
+        //     'id': 'park-boundary',
+        //     'type': 'line',
+        //     'source': 'national-park',
+        //     'layout': {
+        //         'line-join': 'round',
+        //         'line-cap': 'round'
+        //     },
+        //     'paint': {
+        //         'line-color': '#035afc',
+        //         'line-width': 8
+        //     },
+        //     'filter': ['==', '$type', 'LineString']
+        // });
         map.addSource('national-park2', {
             'type': 'geojson',
             'data': street1
@@ -171,39 +182,32 @@ $(document).ready(function () {
             'filter': ['==', '$type', 'LineString']
         });
 
-        map.addSource('tqh-street', {
-            'type': 'geojson',
-            'data': tqh
-        });
-
-        map.addLayer({
-            'id': 'tqh-street-1',
-            'type': 'line',
-            'source': 'tqh-street',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#0fffff',
-                'line-width': 10
-            },
-            'filter': ['==', '$type', 'LineString']
-        });
-
     });
+
+    data_test.forEach(element => {
+        // make a marker for each feature and add to the map
+        let marker = new wemapgl.Marker(marker_real.cloneNode(true))
+            .setLngLat([element.real_lon, element.real_lat])
+            .addTo(map);
+        let viewPopup1 = document.createElement("div");
+        viewPopup1.classList.add("viewPopup2");
+        viewPopup1.innerHTML = element.housenumber
+        let markerElement1 = marker._element;
+        markerElement1.appendChild(viewPopup1);
+
+    })
 
     data.features.forEach(element => {
         if (element.properties.source == "OSM") {
             // make a marker for each feature and add to the map
-            let marker = new wemapgl.Marker(marker_real.cloneNode(true))
-                .setLngLat([element.properties.lon, element.properties.lat])
-                .addTo(map);
-            let viewPopup1 = document.createElement("div");
-            viewPopup1.classList.add("viewPopup2");
-            viewPopup1.innerHTML = element.properties.housenumber
-            let markerElement1 = marker._element;
-            markerElement1.appendChild(viewPopup1);
+            // let marker = new wemapgl.Marker(marker_real.cloneNode(true))
+            //     .setLngLat([element.properties.lon, element.properties.lat])
+            //     .addTo(map);
+            // let viewPopup1 = document.createElement("div");
+            // viewPopup1.classList.add("viewPopup2");
+            // viewPopup1.innerHTML = element.properties.housenumber
+            // let markerElement1 = marker._element;
+            // markerElement1.appendChild(viewPopup1);
 
             let marker2 = new wemapgl.Marker(marker_proj.cloneNode(true))
                 .setLngLat([element.properties.proj_lon, element.properties.proj_lat])
---
+++ pelias-test/data-test.js
@@ -0,0 +1,363 @@
+
+data_test = [
+    {
+        "stt": "1",
+        "housenumber": "7",
+        "real_lat": "21.036323",
+        "real_lon": "105.789173"
+    },
+    {
+        "stt": "2",
+        "housenumber": "9",
+        "real_lat": "21.036270",
+        "real_lon": "105.789090"
+    },
+    {
+        "stt": "3",
+        "housenumber": "14",
+        "real_lat": "21.036550",
+        "real_lon": "105.789330"
+    },
+    {
+        "stt": "4",
+        "housenumber": "16",
+        "real_lat": "21.036570",
+        "real_lon": "105.789240"
+    },
+    {
+        "stt": "5",
+        "housenumber": "17",
+        "real_lat": "21.036270",
+        "real_lon": "105.788840"
+    },
+    {
+        "stt": "6",
+        "housenumber": "19",
+        "real_lat": "21.036270",
+        "real_lon": "105.788800"
+    },
+    {
+        "stt": "7",
+        "housenumber": "27",
+        "real_lat": "21.036310",
+        "real_lon": "105.788590"
+    },
+    {
+        "stt": "8",
+        "housenumber": "29",
+        "real_lat": "21.036290",
+        "real_lon": "105.788560"
+    },
+    {
+        "stt": "9",
+        "housenumber": "37",
+        "real_lat": "21.036320",
+        "real_lon": "105.788490"
+    },
+    {
+        "stt": "10",
+        "housenumber": "39",
+        "real_lat": "21.036320",
+        "real_lon": "105.788470"
+    },
+    {
+        "stt": "11",
+        "housenumber": "42",
+        "real_lat": "21.036620",
+        "real_lon": "105.788130"
+    },
+    {
+        "stt": "12",
+        "housenumber": "44",
+        "real_lat": "21.036620",
+        "real_lon": "105.788110"
+    },
+    {
+        "stt": "13",
+        "housenumber": "45",
+        "real_lat": "21.036320",
+        "real_lon": "105.788390"
+    },
+    {
+        "stt": "14",
+        "housenumber": "46",
+        "real_lat": "21.036660",
+        "real_lon": "105.788000"
+    },
+    {
+        "stt": "15",
+        "housenumber": "47",
+        "real_lat": "21.036320",
+        "real_lon": "105.788350"
+    },
+    {
+        "stt": "16",
+        "housenumber": "49",
+        "real_lat": "21.036320",
+        "real_lon": "105.788290"
+    },
+    {
+        "stt": "17",
+        "housenumber": "51",
+        "real_lat": "21.036320",
+        "real_lon": "105.788260"
+    },
+    {
+        "stt": "18",
+        "housenumber": "52",
+        "real_lat": "21.036660",
+        "real_lon": "105.787940"
+    },
+    {
+        "stt": "19",
+        "housenumber": "53",
+        "real_lat": "21.036320",
+        "real_lon": "105.788130"
+    },
+    {
+        "stt": "20",
+        "housenumber": "58",
+        "real_lat": "21.036660",
+        "real_lon": "105.787880"
+    },
+    {
+        "stt": "21",
+        "housenumber": "61",
+        "real_lat": "21.036320",
+        "real_lon": "105.788080"
+    },
+    {
+        "stt": "22",
+        "housenumber": "62",
+        "real_lat": "21.036660",
+        "real_lon": "105.787840"
+    },
+    {
+        "stt": "23",
+        "housenumber": "63",
+        "real_lat": "21.036320",
+        "real_lon": "105.788050"
+    },
+    {
+        "stt": "24",
+        "housenumber": "65",
+        "real_lat": "21.036320",
+        "real_lon": "105.788010"
+    },
+    {
+        "stt": "25",
+        "housenumber": "66",
+        "real_lat": "21.036660",
+        "real_lon": "105.787780"
+    },
+    {
+        "stt": "26",
+        "housenumber": "67",
+        "real_lat": "21.036320",
+        "real_lon": "105.787980"
+    },
+    {
+        "stt": "27",
+        "housenumber": "69",
+        "real_lat": "21.036320",
+        "real_lon": "105.787940"
+    },
+    {
+        "stt": "28",
+        "housenumber": "70",
+        "real_lat": "21.036630",
+        "real_lon": "105.787650"
+    },
+    {
+        "stt": "29",
+        "housenumber": "81",
+        "real_lat": "21.036380",
+        "real_lon": "105.787730"
+    },
+    {
+        "stt": "30",
+        "housenumber": "91",
+        "real_lat": "21.036380",
+        "real_lon": "105.787500"
+    },
+    {
+        "stt": "31",
+        "housenumber": "93",
+        "real_lat": "21.036380",
+        "real_lon": "105.787450"
+    },
+    {
+        "stt": "32",
+        "housenumber": "95",
+        "real_lat": "21.036380",
+        "real_lon": "105.787400"
+    },
+    {
+        "stt": "33",
+        "housenumber": "97",
+        "real_lat": "21.036380",
+        "real_lon": "105.787370"
+    },
+    {
+        "stt": "34",
+        "housenumber": "99",
+        "real_lat": "21.036380",
+        "real_lon": "105.787340"
+    },
+    {
+        "stt": "35",
+        "housenumber": "101",
+        "real_lat": "21.036380",
+        "real_lon": "105.787310"
+    },
+    {
+        "stt": "36",
+        "housenumber": "109",
+        "real_lat": "21.036380",
+        "real_lon": "105.787180"
+    },
+    {
+        "stt": "37",
+        "housenumber": "111",
+        "real_lat": "21.036380",
+        "real_lon": "105.787160"
+    },
+    {
+        "stt": "38",
+        "housenumber": "113",
+        "real_lat": "21.036380",
+        "real_lon": "105.787130"
+    },
+    {
+        "stt": "39",
+        "housenumber": "115",
+        "real_lat": "21.036370",
+        "real_lon": "105.787080"
+    },
+    {
+        "stt": "40",
+        "housenumber": "117",
+        "real_lat": "21.036370",
+        "real_lon": "105.787030"
+    },
+    {
+        "stt": "41",
+        "housenumber": "119",
+        "real_lat": "21.036380",
+        "real_lon": "105.786980"
+    },
+    {
+        "stt": "42",
+        "housenumber": "121",
+        "real_lat": "21.036400",
+        "real_lon": "105.786930"
+    },
+    {
+        "stt": "43",
+        "housenumber": "123",
+        "real_lat": "21.036380",
+        "real_lon": "105.786860"
+    },
+    {
+        "stt": "44",
+        "housenumber": "125",
+        "real_lat": "21.036370",
+        "real_lon": "105.786800"
+    },
+    {
+        "stt": "45",
+        "housenumber": "127",
+        "real_lat": "21.036380",
+        "real_lon": "105.786500"
+    },
+    {
+        "stt": "46",
+        "housenumber": "129",
+        "real_lat": "21.036370",
+        "real_lon": "105.786230"
+    },
+    {
+        "stt": "47",
+        "housenumber": "137",
+        "real_lat": "21.036470",
+        "real_lon": "105.784600"
+    },
+    {
+        "stt": "48",
+        "housenumber": "139",
+        "real_lat": "21.036480",
+        "real_lon": "105.784580"
+    },
+    {
+        "stt": "49",
+        "housenumber": "141",
+        "real_lat": "21.036470",
+        "real_lon": "105.784530"
+    },
+    {
+        "stt": "50",
+        "housenumber": "143",
+        "real_lat": "21.036480",
+        "real_lon": "105.784480"
+    },
+    {
+        "stt": "51",
+        "housenumber": "145",
+        "real_lat": "21.036490",
+        "real_lon": "105.784440"
+    },
+    {
+        "stt": "52",
+        "housenumber": "147",
+        "real_lat": "21.036480",
+        "real_lon": "105.784410"
+    },
+    {
+        "stt": "53",
+        "housenumber": "149",
+        "real_lat": "21.036480",
+        "real_lon": "105.784370"
+    },
+    {
+        "stt": "54",
+        "housenumber": "159",
+        "real_lat": "21.036480",
+        "real_lon": "105.784210"
+    },
+    {
+        "stt": "55",
+        "housenumber": "161",
+        "real_lat": "21.036490",
+        "real_lon": "105.784190"
+    },
+    {
+        "stt": "56",
+        "housenumber": "163",
+        "real_lat": "21.036490",
+        "real_lon": "105.784150"
+    },
+    {
+        "stt": "57",
+        "housenumber": "165",
+        "real_lat": "21.036470",
+        "real_lon": "105.784130"
+    },
+    {
+        "stt": "58",
+        "housenumber": "167",
+        "real_lat": "21.036480",
+        "real_lon": "105.784000"
+    },
+    {
+        "stt": "59",
+        "housenumber": "175",
+        "real_lat": "21.036480",
+        "real_lon": "105.783700"
+    },
+    {
+        "stt": "60",
+        "housenumber": "177",
+        "real_lat": "21.036490",
+        "real_lon": "105.783660"
+    }
+]
